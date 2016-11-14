904a25b1fc4d7b489ccd7b391cdb2c9c41e5bd9a
Christopher Lee <chmalee@ucsc.edu>
2016-11-14 15:55:14 -0800

    Selenium script now checks hgTracks, hgGene, hgBlat, hgPcr, hgConvert, hgLiftOver, and loading a custom track. Still needs work to check a hub

--- genomeAsiaFromSelenium.py
+++ genomeAsiaFromSelenium.py
@@ -25,43 +25,29 @@ class GenomeAsiaFromSelenium(unittest.TestCase):
         driver.find_element_by_link_text("Home").click()
         driver.find_element_by_link_text("Genomes").click()
         driver.get("http://genome.ucsc.edu/cgi-bin/cartReset")
-        #driver.get("http://genome.ucsc.edu/cgi-bin/hgGateway")
         Select(driver.find_element_by_id("selectAssembly")).select_by_visible_text("Feb. 2009 (GRCh37/hg19)")
         driver.find_element_by_css_selector("div.jwGoButton").click()
+        self.driver.implicitly_wait(1000)
         driver.find_element_by_xpath("//td[@id='td_data_knownGene']/div[2]/map/area[5]").click()
         driver.get("http://genome.ucsc.edu/cgi-bin/hgGateway?db=mm10")
-        
+       
+        # check mm10 hgTracks and hgGene 
         self.driver.implicitly_wait(1000)
-        # below code taken from: 
-        # http://stackoverflow.com/questions/27934945/selenium-move-to-element-does-not-always-mouse-hover
-        men_menu = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
-        (By.XPATH, "//li[@id='tools1']")))
-        ActionChains(driver).move_to_element(men_menu).perform()
-
-        self.driver.implicitly_wait(1000)
-        # wait for Mouse/mm10 item to appear, then click it
-        mm10= WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//li[@id='tools1']/ul/li[3]")))
-        mm10.click()
-        driver.find_element_by_xpath("//td[@id='td_data_knownGene']/div[2]/map/area[6]").click()
+        self.hover_over_menu(driver, "//li[@id='tools1']", "//li[@id='tools1']/ul/li[3]") # click mm10 from Genomes drop down
+        driver.find_element_by_xpath("//td[@id='td_data_knownGene']/div[2]/map/area[6]").click() # clicks into hgGene
         
+        # test custom track
         driver.get("http://genome.ucsc.edu/cgi-bin/hgGateway?db=hg19")
-        # the following link does not exist? old gateway relic of problem with selenium export?
-        #driver.find_element_by_link_text("Human GRCh37/hg19").click()
         self.driver.implicitly_wait(1000)
-        men_menu = WebDriverWait(driver,10).until(EC.visibility_of_element_located((
-        By.XPATH, "//li[@id='myData']")))
-        ActionChains(driver).move_to_element(men_menu).perform() 
-        customTrackLink = WebDriverWait(driver,10).until(EC.visibility_of_element_located((
-        By.XPATH, "//li[@id='myData']/ul/li[4]")))
-        customTrackLink.click()
-
-        #driver.find_element_by_id("customTracksMenuLink").click()
+        self.hover_over_menu(driver, "//li[@id='myData']", "//li[@id='myData']/ul/li[4]")
+        
         driver.find_element_by_name("hgct_customText").clear()
         driver.find_element_by_name("hgct_customText").send_keys("http://hgwdev.cse.ucsc.edu/~brianlee/customTracks/examples.WITHOUT.FTPS.txt")
         driver.find_element_by_name("Submit").click()
         self.driver.implicitly_wait(1000)
         driver.find_element_by_name("submit").click()
-        """driver.get("http://genome.ucsc.edu/cgi-bin/cartReset")
+        """
+        driver.get("http://genome.ucsc.edu/cgi-bin/cartReset")
         driver.find_element_by_link_text("Genome Browser").click()
         driver.find_element_by_id("trackHubsMenuLink").click()
         driver.find_element_by_link_text("My Hubs").click()
@@ -78,33 +64,49 @@ class GenomeAsiaFromSelenium(unittest.TestCase):
         driver.find_element_by_id("cartResetMenuLink").click()
         driver.find_element_by_id("blatMenuLink").click()
         Select(driver.find_element_by_css_selector("select[name=\"db\"]")).select_by_visible_text("Feb. 2009 (GRCh37/hg19)")
+        """
+        driver.get("http://genome.ucsc.edu/cgi-bin/cartReset")
+        driver.get("http://genome.ucsc.edu/cgi-bin/hgGateway?db=hg19")
+        self.hover_over_menu(driver, "//li[@id='tools3']", "//li[@id='tools3']/ul/li[1]") # hgBlat
         driver.find_element_by_name("userSeq").clear()
         driver.find_element_by_name("userSeq").send_keys("AACAAAATCAAACTGTTTTTGTTGGACAATTCTCTGTTAAGCAGCTATAA\\nGCTGAATGACATTAACCGCAAAATGTAACCATAAAGGCCATAAACCCGAC\\nATTGTTAATTAATTAAATGCCTCATTAACTTTTTTAAAAACATGATTTAT\\nTCGATTCATAGAAAACTTAACCATCACTACTAAATGCACACACATGCGGT\\nTCCACATTGGCATCTTAGCCTAAGAACAGACAGGTTCAACTGTAACTGGC\\nCTTTCAGGTGGTCTATTACAGATCTGAAGACAGAGGGTGTTTCTAAACCT\\nCAAGAACCAGATTAACAGAAAACAAAGCTTGAGCAGCCTTTTTATTGCAT\\nGTGGTATCTTTTTAGCTAAGCAGAAGACAATGATAAAGAGGGGTTTTGGG\\nAAACCTCTCCCAAAGCTGTGCATTCATACCGTACCTTATCCTGTTAAGCA\\nAACTGTTCTTTTATTTTAAAGGGTTTACACTGCCACATCTGAATGGACTA")
         driver.find_element_by_name("Submit").click()
+        self.driver.implicitly_wait(1000)
         driver.find_element_by_link_text("browser").click()
         driver.find_element_by_id("p_btn_hgUserPsl").click()
-        driver.find_element_by_id("cartResetMenuLink").click()
-        driver.find_element_by_id("ispMenuLink").click()
+        self.driver.implicitly_wait(1000)
+        self.hover_over_menu(driver, "//li[@id='tools2']", "//li[@id='tools2']/ul/li[3]") # cart reset
+        self.hover_over_menu(driver, "//li[@id='tools3']", "//li[@id='tools3']/ul/li[7]") # hgPcr
         driver.find_element_by_name("wp_f").clear()
         driver.find_element_by_name("wp_f").send_keys("AACAAAATCAAACTGTTTTTGTTGGACAATTCTCTGTTAAGCAGCTATAA")
         driver.find_element_by_name("wp_r").clear()
         driver.find_element_by_name("wp_r").send_keys("AACTGTTCTTTTATTTTAAAGGGTTTACACTGCCACATCTGAATGGACTA")
         driver.find_element_by_name("wp_flipReverse").click()
         driver.find_element_by_name("Submit").click()
+        self.driver.implicitly_wait(1000)
         driver.find_element_by_link_text("chrX:40059679+40060178").click()
+        self.driver.implicitly_wait(1000)
+        self.hover_over_menu(driver, "//li[@id='tools2']", "//li[@id='tools2']/ul/li[3]") # cart reset
         driver.find_element_by_link_text("Genome Browser").click()
-        driver.find_element_by_id("cartResetMenuLink").click()
-        driver.find_element_by_link_text("Genome Browser").click()
-        driver.find_element_by_id("convertMenuLink").click()
+        self.hover_over_menu(driver, "//li[@id='view']", "//li[@id='view']/ul/li[3]") # hgConvert
         driver.find_element_by_name("hglft_doConvert").click()
         driver.find_element_by_link_text("chr9:136127387-136156434").click()
         driver.find_element_by_css_selector("#tools3 > span").click()
-        driver.find_element_by_id("liftOverMenuLink").click()
+        self.hover_over_menu(driver, "//li[@id='tools3']", "//li[@id='tools3']/ul/li[8]") # hgLiftOver
         driver.find_element_by_name("hglft_userData").clear()
         driver.find_element_by_name("hglft_userData").send_keys("chr21:33,031,597-33,041,570")
-        driver.find_element_by_name("Submit").click()
-        driver.find_element_by_link_text("View Conversions").click()
-        """ 
+ 
+    def hover_over_menu(self, driver, menu_header, menu_item_xpath):
+        """hovers over menubar and clicks an item in a dropdown list"""
+        # below code taken from: 
+        # http://stackoverflow.com/questions/27934945/selenium-move-to-element-does-not-always-mouse-hover
+        men_menu = WebDriverWait(driver,10).until(EC.visibility_of_element_located((
+            By.XPATH, menu_header)))
+        ActionChains(driver).move_to_element(men_menu).perform() 
+        link_to_click = WebDriverWait(driver,10).until(EC.visibility_of_element_located(
+            (By.XPATH, menu_item_xpath)))
+        link_to_click.click()
+
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
