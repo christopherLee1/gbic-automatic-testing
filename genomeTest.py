4f8bfec2e8f2f893e2067616927f63703b4e1572
Christopher Lee <chmalee@ucsc.edu>
2016-11-22 14:03:04 -0800

    Some changes to selenium to accept a url paramter to test

--- genomeTest.py
+++ genomeTest.py
@@ -11,25 +11,26 @@ from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re, sys
 
 class GenomeTest(unittest.TestCase):
-    def setUp(self):
-        """We use Chrome here because Firefox has some issues with hover-selecting"""
+    def __init__(self, testname, host):
+        super(GenomeTest, self).__init__(testname)
+        self.base_url = host
         self.driver = webdriver.Chrome("/Users/christopherLee/Downloads/chromedriver")
         self.driver.implicitly_wait(30)
-        self.base_url = # need to get sys.argv[1] in here
         self.verificationErrors = []
         self.accept_next_alert = True
     
     def test_genome_test(self):
+        #print("url is %s\n" % self.host)
         driver = self.driver
         driver.get(self.base_url + "/index.html")
         driver.find_element_by_link_text("Home").click()
         driver.find_element_by_link_text("Genomes").click()
-        driver.get("http://genome.ucsc.edu/cgi-bin/cartReset")
+        driver.get(self.base_url + "/cgi-bin/cartReset")
         Select(driver.find_element_by_id("selectAssembly")).select_by_visible_text("Feb. 2009 (GRCh37/hg19)")
         driver.find_element_by_css_selector("div.jwGoButton").click()
         self.driver.implicitly_wait(1000)
         driver.find_element_by_xpath("//td[@id='td_data_knownGene']/div[2]/map/area[5]").click()
-        driver.get("http://genome.ucsc.edu/cgi-bin/hgGateway?db=mm10")
+        driver.get(self.base_url + "/cgi-bin/hgGateway?db=mm10")
        
         # check mm10 hgTracks and hgGene 
         self.driver.implicitly_wait(1000)
@@ -37,9 +38,12 @@ class GenomeTest(unittest.TestCase):
         driver.find_element_by_xpath("//td[@id='td_data_knownGene']/div[2]/map/area[6]").click() # clicks into hgGene
         
         # test custom track
-        driver.get("http://genome.ucsc.edu/cgi-bin/hgGateway?db=hg19")
+        driver.get(self.base_url + "/cgi-bin/hgGateway?db=hg19")
         self.driver.implicitly_wait(1000)
-        self.hover_over_menu(driver, "//li[@id='myData']", "//li[@id='myData']/ul/li[4]")
+        if self.base_url[-4:] == "1234": # gbib
+            self.hover_over_menu(driver, "//li[@id='myData']", "//li[@id='myData']/ul/li[5]")
+        else: # any other mirror
+            self.hover_over_menu(driver, "//li[@id='myData']", "//li[@id='myData']/ul/li[4]")
         
         driver.find_element_by_name("hgct_customText").clear()
         driver.find_element_by_name("hgct_customText").send_keys("http://hgwdev.cse.ucsc.edu/~brianlee/customTracks/examples.WITHOUT.FTPS.txt")
@@ -65,8 +69,8 @@ class GenomeTest(unittest.TestCase):
         driver.find_element_by_id("blatMenuLink").click()
         Select(driver.find_element_by_css_selector("select[name=\"db\"]")).select_by_visible_text("Feb. 2009 (GRCh37/hg19)")
         """
-        driver.get("http://genome.ucsc.edu/cgi-bin/cartReset")
-        driver.get("http://genome.ucsc.edu/cgi-bin/hgGateway?db=hg19")
+        driver.get(self.base_url + "/cgi-bin/cartReset")
+        driver.get(self.base_url + "/cgi-bin/hgGateway?db=hg19")
         self.hover_over_menu(driver, "//li[@id='tools3']", "//li[@id='tools3']/ul/li[1]") # hgBlat
         driver.find_element_by_name("userSeq").clear()
         driver.find_element_by_name("userSeq").send_keys("AACAAAATCAAACTGTTTTTGTTGGACAATTCTCTGTTAAGCAGCTATAA\\nGCTGAATGACATTAACCGCAAAATGTAACCATAAAGGCCATAAACCCGAC\\nATTGTTAATTAATTAAATGCCTCATTAACTTTTTTAAAAACATGATTTAT\\nTCGATTCATAGAAAACTTAACCATCACTACTAAATGCACACACATGCGGT\\nTCCACATTGGCATCTTAGCCTAAGAACAGACAGGTTCAACTGTAACTGGC\\nCTTTCAGGTGGTCTATTACAGATCTGAAGACAGAGGGTGTTTCTAAACCT\\nCAAGAACCAGATTAACAGAAAACAAAGCTTGAGCAGCCTTTTTATTGCAT\\nGTGGTATCTTTTTAGCTAAGCAGAAGACAATGATAAAGAGGGGTTTTGGG\\nAAACCTCTCCCAAAGCTGTGCATTCATACCGTACCTTATCCTGTTAAGCA\\nAACTGTTCTTTTATTTTAAAGGGTTTACACTGCCACATCTGAATGGACTA")
@@ -76,7 +80,10 @@ class GenomeTest(unittest.TestCase):
         driver.find_element_by_id("p_btn_hgUserPsl").click()
         self.driver.implicitly_wait(1000)
         self.hover_over_menu(driver, "//li[@id='tools2']", "//li[@id='tools2']/ul/li[3]") # cart reset
-        self.hover_over_menu(driver, "//li[@id='tools3']", "//li[@id='tools3']/ul/li[7]") # hgPcr
+        if self.base_url[-4:] == "1234":
+            self.hover_over_menu(driver, "//li[@id='tools3']", "//li[@id='tools3']/ul/li[6]") # hgPcr
+        else:
+            self.hover_over_menu(driver, "//li[@id='tools3']", "//li[@id='tools3']/ul/li[7]") # hgPcr
         driver.find_element_by_name("wp_f").clear()
         driver.find_element_by_name("wp_f").send_keys("AACAAAATCAAACTGTTTTTGTTGGACAATTCTCTGTTAAGCAGCTATAA")
         driver.find_element_by_name("wp_r").clear()
@@ -92,7 +99,10 @@ class GenomeTest(unittest.TestCase):
         driver.find_element_by_name("hglft_doConvert").click()
         driver.find_element_by_link_text("chr9:136127387-136156434").click()
         driver.find_element_by_css_selector("#tools3 > span").click()
-        self.hover_over_menu(driver, "//li[@id='tools3']", "//li[@id='tools3']/ul/li[8]") # hgLiftOver
+        if self.base_url[-4:] == "1234":
+            self.hover_over_menu(driver, "//li[@id='tools3']", "//li[@id='tools3']/ul/li[7]") # hgLiftOver
+        else:
+            self.hover_over_menu(driver, "//li[@id='tools3']", "//li[@id='tools3']/ul/li[8]") # hgLiftOver
         driver.find_element_by_name("hglft_userData").clear()
         driver.find_element_by_name("hglft_userData").send_keys("chr21:33,031,597-33,041,570")
  
@@ -133,4 +143,6 @@ class GenomeTest(unittest.TestCase):
         self.assertEqual([], self.verificationErrors)
 
 if __name__ == "__main__":
-    unittest.main(argv=[sys.argv[1]]) # argv[1] is url like http://abc.xyz
+    suite = unittest.TestSuite()
+    suite.addTest(GenomeTest("test_genome_test", sys.argv[1]))
+    unittest.TextTestRunner().run(suite)
