679d6b4a3caf4cfa8257593d7bb19fb3b2987d1d
Christopher Lee <chmalee@ucsc.edu>
2016-12-19 16:23:27 -0800

    Adding a little cart_reset method

--- genomeTest.py
+++ genomeTest.py
@@ -25,7 +25,7 @@ class GenomeTest(unittest.TestCase):
         driver.get(self.base_url + "/index.html")
         driver.find_element_by_link_text("Home").click()
         driver.find_element_by_link_text("Genomes").click()
-        driver.get(self.base_url + "/cgi-bin/cartReset")
+        self.cart_reset(driver)
         Select(driver.find_element_by_id("selectAssembly")).select_by_visible_text("Feb. 2009 (GRCh37/hg19)")
         driver.find_element_by_css_selector("div.jwGoButton").click()
         self.driver.implicitly_wait(1000)
@@ -51,7 +51,7 @@ class GenomeTest(unittest.TestCase):
         self.driver.implicitly_wait(1000)
         driver.find_element_by_name("submit").click()
         """
-        driver.get("http://genome.ucsc.edu/cgi-bin/cartReset")
+        self.cart_reset(driver)
         driver.find_element_by_link_text("Genome Browser").click()
         driver.find_element_by_id("trackHubsMenuLink").click()
         driver.find_element_by_link_text("My Hubs").click()
@@ -65,11 +65,11 @@ class GenomeTest(unittest.TestCase):
         Select(driver.find_element_by_name("hub_77624_bam1")).select_by_visible_text("full")
         driver.find_element_by_name("hub_77624_bam1.doWiggle").click()
         driver.find_element_by_name("Submit").click()
-        driver.find_element_by_id("cartResetMenuLink").click()
+        self.cart_reset(driver)
         driver.find_element_by_id("blatMenuLink").click()
         Select(driver.find_element_by_css_selector("select[name=\"db\"]")).select_by_visible_text("Feb. 2009 (GRCh37/hg19)")
         """
-        driver.get(self.base_url + "/cgi-bin/cartReset")
+        self.cart_reset(driver)
         driver.get(self.base_url + "/cgi-bin/hgGateway?db=hg19")
         self.hover_over_menu(driver, "//li[@id='tools3']", "//li[@id='tools3']/ul/li[1]") # hgBlat
         driver.find_element_by_name("userSeq").clear()
@@ -79,7 +79,7 @@ class GenomeTest(unittest.TestCase):
         driver.find_element_by_link_text("browser").click()
         driver.find_element_by_id("p_btn_hgUserPsl").click()
         self.driver.implicitly_wait(1000)
-        self.hover_over_menu(driver, "//li[@id='tools2']", "//li[@id='tools2']/ul/li[3]") # cart reset
+        self.cart_reset(driver)
         if self.base_url[-4:] == "1234":
             self.hover_over_menu(driver, "//li[@id='tools3']", "//li[@id='tools3']/ul/li[6]") # hgPcr
         else:
@@ -93,7 +93,7 @@ class GenomeTest(unittest.TestCase):
         self.driver.implicitly_wait(1000)
         driver.find_element_by_link_text("chrX:40059679+40060178").click()
         self.driver.implicitly_wait(1000)
-        self.hover_over_menu(driver, "//li[@id='tools2']", "//li[@id='tools2']/ul/li[3]") # cart reset
+        self.cart_reset(driver)
         driver.find_element_by_link_text("Genome Browser").click()
         self.hover_over_menu(driver, "//li[@id='view']", "//li[@id='view']/ul/li[3]") # hgConvert
         driver.find_element_by_name("hglft_doConvert").click()
@@ -105,7 +105,7 @@ class GenomeTest(unittest.TestCase):
             self.hover_over_menu(driver, "//li[@id='tools3']", "//li[@id='tools3']/ul/li[8]") # hgLiftOver
         driver.find_element_by_name("hglft_userData").clear()
         driver.find_element_by_name("hglft_userData").send_keys("chr21:33,031,597-33,041,570")
- 
+
     def hover_over_menu(self, driver, menu_header, menu_item_xpath):
         """hovers over menubar and clicks an item in a dropdown list"""
         # below code taken from: 
@@ -116,6 +116,9 @@ class GenomeTest(unittest.TestCase):
         link_to_click = WebDriverWait(driver,10).until(EC.visibility_of_element_located(
             (By.XPATH, menu_item_xpath)))
         link_to_click.click()
+    def cart_reset(self, driver):
+        # hovers over the Genome Browser menubar item and clicks cart reset
+        self.hover_over_menu(driver, "//li[@id='tools2']", "//li[@id='tools2']/ul/li[3]") 
 
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
